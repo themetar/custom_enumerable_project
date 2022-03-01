80753f0c9300a5cbe480bdf7e12e1fc3b086fb02
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2022-03-01 17:43:54 +0100

    Add Array#my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -2,10 +2,18 @@ module Enumerable
   # Your code goes here
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  # Yields to block for every element of the array
+  def my_each
+    if block_given?
+      i = 0
+      while i < self.length
+        yield self[i]
+
+        i += 1
+      end
+    end
+
+    self  # return self
+  end
 end
